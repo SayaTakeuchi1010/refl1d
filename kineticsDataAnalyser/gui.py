3741ccf0952cd580c768b4fc70c151cc3560daf3
SayaTakeuchi1010 <saya.takeuchi@nist.gov>
2021-05-03 16:59:33 -0400

    x, y label added, check button works

--- kineticsDataAnalyser/gui.py
+++ kineticsDataAnalyser/gui.py
@@ -10,29 +10,31 @@ qz_0 = rr.dataList[0][0]
 int_0 = rr.dataList[0][1]
 qz_1 = rr.dataList[1][0]
 int_1 = rr.dataList[1][1]
-fig_QvsI = plt.figure(figsize= (5, 4))
-plt.xlabel = ('Qz')
-plt.ylabel = ('intensity')
-plt.plot(qz_0, int_0, label='entry0')
-plt.plot(qz_1, int_1, label='entry1')
-plt.legend(loc=4)
+fig_1 = plt.figure(figsize=(5, 4))
+axes_1 = fig_1.add_axes([0.2, 0.2, 0.6, 0.6])
+axes_1.set_xlabel('Qz')
+axes_1.set_ylabel('intensity')
+plt.ticklabel_format(axis='both', style = 'scientific' )
+axes_1.plot(qz_0, int_0, label='entry0')
+axes_1.plot(qz_1, int_1, label='entry1')
+axes_1.legend(loc=4)
 
-#
-# t = np.arange(0.0, 2.0, 0.01)
-# s0 = np.sin(2*np.pi*t)
-# s1 = np.sin(4*np.pi*t)
-# s2 = np.sin(6*np.pi*t)
-#
-# fig, ax = plt.subplots()
-# l0, = ax.plot(t, s0, visible=False, lw=2, color='k', label='2 Hz')
-# l1, = ax.plot(t, s1, lw=2, color='r', label='4 Hz')
+
+fig, ax = plt.subplots()
+ax.set_xlabel('Qz')
+ax.set_ylabel('intensity')
+plt.ticklabel_format(axis='both', style='scientific' )
+
+l0, = ax.plot(qz_0, int_0, lw=2, color='b', marker='.', label='entry0')
+l1, = ax.plot(qz_1, int_1, lw=2, color='r', marker='.', label='entry1')
+plt.legend(loc=4)
 # l2, = ax.plot(t, s2, lw=2, color='g', label='6 Hz')
 # plt.subplots_adjust(left=0.2)
 #
-lines = [l0, l1, l2]
+lines = [l0, l1]
 
 # Make checkbuttons with all plotted lines with correct visibility
-rax = plt.axes([0.05, 0.4, 0.1, 0.15])
+rax = plt.axes([0.9, 0.2, 0.1, 0.15])
 labels = [str(line.get_label()) for line in lines]
 visibility = [line.get_visible() for line in lines]
 check = CheckButtons(rax, labels, visibility)
