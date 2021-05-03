400bca99dcea4aef1e3e218e7fff112c3c33a588
SayaTakeuchi1010 <saya.takeuchi@nist.gov>
2021-05-03 16:41:58 -0400

    dataList is appended in float not string

--- kineticsDataAnalyser/readRefl1d.py
+++ kineticsDataAnalyser/readRefl1d.py
@@ -67,7 +67,21 @@ class ReadRefl1d:
         # test = fullText[10:20]
         # print('test', test)
         print('individualList', individualList)
-        dataList.append(individualList)
+        # print('type inlist', type(individualList[0][0]))
+
+        # change dataList from strings to floats
+        individualListInFloat = []
+        for b in range(len(individualList)):
+            oneRowInFloat = []
+            for c in range(4):
+                oneDataInARow = float(individualList[b][c])
+                oneRowInFloat.append(oneDataInARow)
+            individualListInFloat.append(oneRowInFloat)
+        print('individualListInFloat', individualListInFloat)
+        dataList.append(individualListInFloat)
+
+
+
 
     # 2(S1-S2)/(E1+E2)  where S is specular intensity and E is SQRT(S)
     # print('dataList', dataList)
@@ -82,9 +96,9 @@ class ReadRefl1d:
             if b <= len(dataList[a]):
                 print('a', a)
                 print('b', b)
-                S1 = float(dataList[a][b][1])
+                S1 = dataList[a][b][1]
                 # S1 = dataList[a][b][1]
-                S2 = float(dataList[a + 1][b][1])
+                S2 = dataList[a + 1][b][1]
                 E1 = math.sqrt(S1)
                 E2 = math.sqrt(S2)
                 # print('S1, S2', S1, S2)
@@ -104,119 +118,6 @@ class ReadRefl1d:
                 a + 1
             print('indivisualResiduals', indivisualResiduals)
             print('individualRelativeDifferences', individualRelativeDifferences)
-
-
-
-#
-# # fulltext = []
-# with open(filePath) as f:
-#     lines = f.readlines()
-#     # print(lines)
-# #     fulltext.append(lines)
-# # print('fulltext', fulltext)
-# # print(len(fulltext))
-#
-# # this fails with space in a string ("title":"cdr slit")
-# text = open(filePath, 'r')
-# fullText = [line.split(' ') for line in text.readlines()]
-# print('fullText', fullText)
-# print(len(fullText))
-#
-# rfl1dData = []
-# word1 = 'name'
-# word2 = 'units'
-# nameRowNumbers = []
-# unitRowNumbers = []
-# rowNumberDataStarts = []
-# rowNumberDataEnds = []
-# totalRow=0
-# for i, line in enumerate(lines):
-#     if word1 in line:
-#         print(word1)
-#         print(i)
-#         firstRowOfData = i+1
-#         # print(firstRowOfData)
-#         nameRowNumbers.append(i)
-#         endOfData = i-3
-#         print('endOfData', endOfData)
-#         if endOfData > 10:
-#             # get endOfData from the data set "entry" 2 and above,
-#             # "entry" 1 results in endOfData = -2 and does not get appended
-#             rowNumberDataEnds.append(endOfData)
-#
-#     # one row after "units" is the start of data set
-#     if word2 in line:
-#         print('line of word2')
-#         print(i)
-#         unitRowNumbers.append(i)
-#         dataStarts = i+1
-#         rowNumberDataStarts.append(dataStarts)
-#     totalRow = totalRow + 1
-#
-# print((totalRow))
-# rowNumberDataEnds.append(totalRow)
-#
-# print('nameRowNumbers', nameRowNumbers)
-# print('unitRowNumbers', unitRowNumbers)
-# print('rowNumberDataEnds', rowNumberDataEnds)
-# print('rowNumberDataStarts', rowNumberDataStarts)
-#
-# dataList = []
-# for a in range(len(rowNumberDataStarts)):
-#     print('a', a)
-#     startRow = rowNumberDataStarts[a]
-#     print('startRow', startRow)
-#     print(type(startRow))
-#     endRow = rowNumberDataEnds[a]+1
-#     individualList = fullText[startRow: endRow]
-#     # test = fullText[10:20]
-#     # print('test', test)
-#     print('individualList', individualList)
-#     dataList.append(individualList)
-#
-#
-# # 2(S1-S2)/(E1+E2)  where S is specular intensity and E is SQRT(S)
-# # print('dataList', dataList)
-# print('len((dataList))', len(dataList))
-# while a in range(len(dataList)-1):
-#     print('datalist[a]', dataList[a])
-#     print('datalist[a+1]', dataList[a+1])
-#     indivisualResiduals = []
-#     individualRelativeDifferences = []
-#
-#     for b in range(len(dataList[a])):
-#         if b <= len(dataList[a]):
-#             print('a', a)
-#             print('b', b)
-#             S1 = float(dataList[a][b][1])
-#             # S1 = dataList[a][b][1]
-#             S2 = float(dataList[a+1][b][1])
-#             E1 = math.sqrt(S1)
-#             E2 = math.sqrt(S2)
-#             # print('S1, S2', S1, S2)
-#             print('S1, S2, E1, E2', S1, S2, E1, E2)
-#
-#             try:
-#                 residual = 2*(S1-S2)/(E1+E2)
-#                 relativeDifference = 2*(S1-S2)/(S1+S2)
-#                 indivisualResiduals.append(residual)
-#                 individualRelativeDifferences.append(relativeDifference)
-#             except:
-#                 # if E1+E2 = 0 or S1+S2 = 0
-#                 indivisualResiduals.append('value zero')
-#                 individualRelativeDifferences.append('value zero')
-#
-#         else:
-#             a+1
-#         print('indivisualResiduals', indivisualResiduals)
-#         print('individualRelativeDifferences', individualRelativeDifferences)
-#
-
-
-
-
-
-
 
 
 
