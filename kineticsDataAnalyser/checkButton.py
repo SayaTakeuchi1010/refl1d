109a1ac6d0a34824ee5cef895ba5e5beec1f2aef
SayaTakeuchi1010 <saya.takeuchi@nist.gov>
2021-05-07 15:48:55 -0400

    __init__ created, doesn't work

--- kineticsDataAnalyser/checkButton.py
+++ kineticsDataAnalyser/checkButton.py
@@ -1,26 +1,23 @@
 import matplotlib.pyplot as plt
 from matplotlib.widgets import CheckButtons
 from matplotlib.widgets import TextBox
-from readRefl1d import ReadRefl1d as rr
 from itertools import cycle
 
-# def __init__():
+def __init__(self, readRef):
+    # get list from readRefl1d
+    dataInFloat = rr.dataList
+    sampleName = rr.sampleName
+    rr = readRef
 
-
-
-# get list from readRefl1d
-dataInFloat = rr.dataList
-sampleName = rr.sampleName
-
-dataToPlot = []
-for a in range(len(dataInFloat)):
-    print('a', a)
-    qzIntUncertainty = []
-    # create list of Qz
-    listOfQz = []
-    for i in range(len(dataInFloat[a])):
-        listOfQz.append(dataInFloat[a][i][0])
-    print('listOfQz', listOfQz)
+    dataToPlot = []
+    for a in range(len(dataInFloat)):
+        print('a', a)
+        qzIntUncertainty = []
+        # create list of Qz
+        listOfQz = []
+        for i in range(len(dataInFloat[a])):
+            listOfQz.append(dataInFloat[a][i][0])
+        print('listOfQz', listOfQz)
 
     listOfInt = []
     for i in range(len(dataInFloat[a])):
@@ -37,114 +34,123 @@ for a in range(len(dataInFloat)):
     qzIntUncertainty.append(listOfUncertainty)
     dataToPlot.append(qzIntUncertainty)
 
-colors = cycle(["aqua", "black", "blue", "fuchsia", "gray", "green", "lime", "maroon", "navy", "olive", "purple", "red", "silver", "teal", "yellow"])
+    colors = cycle(
+        ["aqua", "black", "blue", "fuchsia", "gray", "green", "lime", "maroon", "navy", "olive", "purple", "red",
+         "silver", "teal", "yellow"])
 
-fig_1, ax_1 = plt.subplots(figsize = (8, 4), nrows=2, ncols=3)
-ax_1[0, 0].set_title('Qz vs. Intensity')
-ax_1[0, 0].set_xlabel('Qz')
-ax_1[0, 0].set_ylabel('intensity')
-ax_1[0, 0].ticklabel_format(axis='both', style='scientific')
-ax_1[0, 0].semilogy()
-plt.legend(loc='best')
+    fig_1, ax_1 = plt.subplots(figsize=(8, 4), nrows=2, ncols=3)
+    ax_1[0, 0].set_title('Qz vs. Intensity')
+    ax_1[0, 0].set_xlabel('Qz')
+    ax_1[0, 0].set_ylabel('intensity')
+    ax_1[0, 0].ticklabel_format(axis='both', style='scientific')
+    ax_1[0, 0].semilogy()
+    plt.legend(loc='best')
 
+    allplotsData = []
+    allplotsErrorbar = []
 
-allplotsData = []
-allplotsErrorbar = []
+    # plot thw whole entry(0 ~ n) in one plot
+    for i in range(len(dataToPlot)):
+        lineiData = ax_1[0, 0].plot(dataToPlot[i][0], dataToPlot[i][1], label='entry' + str(i), color=next(colors),
+                                    marker='.')
+        # print('lineiData', lineiData)
+        allplotsData.append(lineiData)
+        # errori = ax_1[0, 0].errorbar(dataToPlot[i][0], dataToPlot[i][1], label='entry' + str(i), yerr=dataToPlot[i][2], color=next(colors))
+        # print('errori', errori)
+        # allplotsErrorbar.append(errori)
+        # add 'color, entry i' box in left panel
+        ax_1[0, 0].legend(loc='best')
 
-# plot thw whole entry(0 ~ n) in one plot
-for i in range(len(dataToPlot)):
-    lineiData = ax_1[0, 0].plot(dataToPlot[i][0], dataToPlot[i][1], label='entry' + str(i), color=next(colors), marker='.')
-    # print('lineiData', lineiData)
-    allplotsData.append(lineiData)
-    # errori = ax_1[0, 0].errorbar(dataToPlot[i][0], dataToPlot[i][1], label='entry' + str(i), yerr=dataToPlot[i][2], color=next(colors))
-    # print('errori', errori)
-    # allplotsErrorbar.append(errori)
-    # add 'color, entry i' box in left panel
-    ax_1[0, 0].legend(loc='best')
+        # get list of labels
+        labels = []
+        for i in range(len(dataToPlot)):
+            label = sampleName + '_entry' + str(i)
+            labels.append(label)
 
+        # make check box inside panel 2 (location 1 in figsize = (8, 4), nrows=1, ncols=3)
+        rax = ax_1[0, 1]
 
-# get list of labels
-labels = []
-for i in range(len(dataToPlot)):
-    label = sampleName + '_entry' + str(i)
-    labels.append(label)
+        activated = []
+        for i in range(len(dataToPlot)):
+            activated.append(True)
+        # print('activated', activated)
 
-# make check box inside panel 2 (location 1 in figsize = (8, 4), nrows=1, ncols=3)
-rax = ax_1[0, 1]
-
-activated = []
-for i in range(len(dataToPlot)):
-    activated.append(True)
-# print('activated', activated)
-
-chxbox = CheckButtons(rax, labels, activated)
-
-def set_visible(label):
-    index = labels.index(label)
-    # allplotsData[index][0]: get inside of the list to get <class 'matplotlib.lines.Line2D'>
-    # print('allplotsData[index]', allplotsData[index][0])
-    # print(type(allplotsData[index][0]))
-    allplotsData[index][0].set_visible(not allplotsData[index][0].get_visible())
-
-    ### may need to handle inside the order in artist to deal with error bar ###
-    # https://stackoverflow.com/questions/19470104/python-matplotlib-errorbar-legend-picking
-    # print('allplotsErrorbar[index]', allplotsErrorbar[index])
-    # print(type(allplotsErrorbar[index]))
-    # allplotsErrorbar[index].set_visible(not allplotsErrorbar[index].get_visible())
-
-    plt.draw()
-
-chxbox.on_clicked(set_visible)
-
-textBoxLocation = fig_1.add_axes([0.05, 0.05, 0.05, 0.05])
-### make sure input has comma after number for single entry ###
-textBox = TextBox(textBoxLocation, 'input')
+        chxbox = CheckButtons(rax, labels, activated)
 
 
-def submit(expression):
-    ax_1[1, 0].clear()
-    selectedEntry = list(eval(expression))
-    print('selectedEntry', selectedEntry)
-    print(type(selectedEntry))
+        def set_visible(label):
+            index = labels.index(label)
+            # allplotsData[index][0]: get inside of the list to get <class 'matplotlib.lines.Line2D'>
+            # print('allplotsData[index]', allplotsData[index][0])
+            # print(type(allplotsData[index][0]))
+            allplotsData[index][0].set_visible(not allplotsData[index][0].get_visible())
 
-    selectedPlotsData = []
-    for i in range(len(selectedEntry)):
-        print('i in submit for loop', i)
-        print('selectedEntry[i]', selectedEntry[i])
-        erroriData = ax_1[1, 0].errorbar(dataToPlot[selectedEntry[i]][0], dataToPlot[selectedEntry[i]][1], label='entry' + str(selectedEntry[i]), yerr=dataToPlot[selectedEntry[i]][2], color=next(colors))
-        selectedPlotsData.append(erroriData)
-        ax_1[1, 0].legend(loc='best')
+            ### may need to handle inside the order in artist to deal with error bar ###
+            # https://stackoverflow.com/questions/19470104/python-matplotlib-errorbar-legend-picking
+            # print('allplotsErrorbar[index]', allplotsErrorbar[index])
+            # print(type(allplotsErrorbar[index]))
+            # allplotsErrorbar[index].set_visible(not allplotsErrorbar[index].get_visible())
+
+            plt.draw()
+
+        chxbox.on_clicked(set_visible)
+
+    textBoxLocation = fig_1.add_axes([0.05, 0.05, 0.05, 0.05])
+    ### make sure input has comma after number for single entry ###
+    textBox = TextBox(textBoxLocation, 'input')
 
     ax_1[1, 0].set_title('Qz vs. Intensity (error bar)')
     ax_1[1, 0].set_xlabel('Qz ')
     ax_1[1, 0].set_ylabel('intensity ')
-    # error below
-    # ax_1[1, 0].ticklabel_format(axis='both', style='scientific')
     ax_1[1, 0].semilogy()
-    # ax_1[1, 0].autoscale_view()
 
-    # below does not work
-    # fig.subplots_adjust(bottom=0.5)
-    plt.draw()
+    # show residual plot
+    entryForResidualsLocation = fig_1.add_axes([0.5, 0.05, 0.05, 0.05])
+    ### make sure input has comma after number for single entry ###
+    entryForResiduals = TextBox(entryForResidualsLocation, 'selected entry number')
+    ax_1[1, 1].set_title('Qz vs. Residual')
+    ax_1[1, 0].set_xlabel('Qz ')
+    ax_1[1, 0].set_ylabel('Residual: 2(S1-S2)/(E1 + E2)')
 
-textBox.on_submit(submit)
+    def submit(expression):
+        ax_1[1, 0].clear()
+        selectedEntry = list(eval(expression))
+        print('selectedEntry', selectedEntry)
+        print(type(selectedEntry))
+
+        selectedPlotsData = []
+        for i in range(len(selectedEntry)):
+            print('i in submit for loop', i)
+            print('selectedEntry[i]', selectedEntry[i])
+            erroriData = ax_1[1, 0].errorbar(dataToPlot[selectedEntry[i]][0], dataToPlot[selectedEntry[i]][1],
+                                             label='entry' + str(selectedEntry[i]),
+                                             yerr=dataToPlot[selectedEntry[i]][2], color=next(colors))
+            selectedPlotsData.append(erroriData)
+            ax_1[1, 0].legend(loc='best')
+
+        # error below
+        # ax_1[1, 0].ticklabel_format(axis='both', style='scientific')
+
+        # ax_1[1, 0].autoscale_view()
+
+        # below does not work
+        # fig.subplots_adjust(bottom=0.5)
+        plt.draw()
+
+    textBox.on_submit(submit)
+
+    def getEntryNumber(expression):
+        entryNumberForResidual = list(eval(expression))
+        print('entryNumberForResidual', entryNumberForResidual)
+        print(type(entryNumberForResidual))
+        return entryNumberForResidual
+
+    entryForResiduals.on_submit(getEntryNumber)
+
+    plt.show()
 
 
-# show residual plot
-entryForResidualsLocation = fig_1.add_axes([0.5, 0.05, 0.05, 0.05])
-### make sure input has comma after number for single entry ###
-entryForResiduals = TextBox(entryForResidualsLocation, 'selected entry number')
-ax_1[1, 1].set_title('Qz vs. Residual')
-ax_1[1, 0].set_xlabel('Qz ')
-ax_1[1, 0].set_ylabel('Residual: 2(S1-S2)/(E1 + E2)')
 
-def getEntryNumber(expression):
-    entryNumberForResidual = list(eval(expression))
-    print('entryNumberForResidual', entryNumberForResidual)
-    print(type(entryNumberForResidual))
-    return entryNumberForResidual
-
-entryForResiduals.on_submit(getEntryNumber)
 
 
 
@@ -173,4 +179,3 @@ entryForResiduals.on_submit(getEntryNumber)
 
 # chxbox.on_clicked(set_visible)
 
-plt.show()
