bc6dd366f1da1151771dc7ded4b3ed03d09337d0
SayaTakeuchi1010 <saya.takeuchi@nist.gov>
2021-05-17 10:22:44 -0400

    code clean up, folder directory changed for combining data

--- kineticsDataAnalyser/checkButton.py
+++ kineticsDataAnalyser/checkButton.py
@@ -3,42 +3,53 @@ from matplotlib.widgets import CheckButtons
 from matplotlib.widgets import TextBox
 from readRefl1d import ReadRefl1d as rr
 from itertools import cycle
-import math
 import os
 
-# get list from readRefl1d
+# get list of float data and sampleName from readRefl1d
 dataInFloat = rr.dataList
 sampleName = rr.sampleName
 
 
-### plot original data in ax_1[0, 0] ###
+### plot original data in fig_1, ax_1[0]  ###
+
+# dataInFloat format is [[Qz, intensity, uncertainty], [Qz, intensity, uncertainty]...]
+# change format to [[row1 Qz, row2 Qz,,,][row1 int, row2 int,,,][row1 uncertainty, row2 uncertainty,,]],,,,
+
 dataToPlot = []
 for a in range(len(dataInFloat)):
-    # print('a', a)
+    # empty list to append Qz, Intensity and uncertainty for one entry
     qzIntUncertainty = []
-    # create list of Qz
+
+    # create empty list of Qz
     listOfQz = []
     for i in range(len(dataInFloat[a])):
+        # append Qz value in
         listOfQz.append(dataInFloat[a][i][0])
-    # print('listOfQz', listOfQz)
 
+    # create empty list of Intensity
     listOfInt = []
     for i in range(len(dataInFloat[a])):
         listOfInt.append(dataInFloat[a][i][1])
-    # print('listOfInt', listOfInt)
 
+    # create empty list of uncertainty
     listOfUncertainty = []
     for i in range(len(dataInFloat[a])):
         listOfUncertainty.append(dataInFloat[a][i][2])
-    # print('listOfUncertainty', listOfUncertainty)
 
+    # listOfQz format: [row1 Qz, row2 Qz,,,]
+    # lisfOfInt format: [row1 int, row2 int,,,]
+    # listOfUncertainty format: [row1 uncertainty, row2 uncertainty,,]
     qzIntUncertainty.append(listOfQz)
     qzIntUncertainty.append(listOfInt)
     qzIntUncertainty.append(listOfUncertainty)
+    # qzIntUncertainty format: [row1 Qz, row2 Qz,,,][row1 int, row2 int,,,][row1 uncertainty, row2 uncertainty,,] of entry#
     dataToPlot.append(qzIntUncertainty)
+    # dataToPlot format: [[entry0 qzIntUncertainty], [entry1 qzIntUncertainty],,,]
 
+# list of color that python would go through. It has more than 10, and probably don't use everything.
 colors = cycle(["aqua", "black", "blue", "fuchsia", "gray", "green", "lime", "maroon", "navy", "olive", "purple", "red", "silver", "teal", "yellow"])
 
+# fig1 has Qz vs Intensity on left panel, check box with list of original entries in right box
 fig_1, ax_1 = plt.subplots(nrows=1, ncols=2)
 ax_1[0].set_title('Qz vs. Intensity')
 ax_1[0].set_xlabel('Qz')
@@ -47,102 +58,88 @@ ax_1[0].ticklabel_format(axis='both', style='scientific')
 ax_1[0].semilogy()
 
 
-allplotsData = []
+allplotsMatplotlib = []
 allplotsErrorbar = []
 
 # TODO take 'entry' from .refl do not append with list order
 # plot thw whole entry(0 ~ n) in one plot
 for i in range(len(dataToPlot)):
-    lineiData = ax_1[0].plot(dataToPlot[i][0], dataToPlot[i][1], label='entry' + str(i), color=next(colors), marker='.')
+    lineiMatplotlib = ax_1[0].plot(dataToPlot[i][0], dataToPlot[i][1], label='entry' + str(i), color=next(colors), marker='.')
     # print('lineiData', lineiData)
-    allplotsData.append(lineiData)
+    allplotsMatplotlib.append(lineiMatplotlib)
     # add 'color, entry i' box in left panel
     ax_1[0].legend(loc='best', fontsize='small')
 
 
-### plot original data in ax_1[0, 0] end ###
+### plot original data in ax_1[0] end ###
 
 
-### check buttons to make visible/not visible in ax_1[0, 1] ###
+### check buttons to make visible/not visible in fig_1 ax_1[1] ###
 
-# get list of labels
+# crate list of labels
 labels = []
 for i in range(len(dataToPlot)):
     label = sampleName + '_entry' + str(i)
     labels.append(label)
 
 
-# make check box inside panel 2 (location 1 in figsize = (8, 4), nrows=1, ncols=3)
-rax = ax_1[ 1]
+# make check box inside panel 2 (location 2 in nrows=1, ncols=2)
+rax = ax_1[1]
 
 rax.set_title('List of data')
 
 activated = []
 for i in range(len(dataToPlot)):
     activated.append(True)
-# print('activated', activated)
 
 chxbox = CheckButtons(rax, labels, activated)
 
+# function to go in for check box
 def set_visible(label):
     index = labels.index(label)
-    # print('label', label)
-    # print('labels', labels)
-    # allplotsData[index][0]: get inside of the list to get <class 'matplotlib.lines.Line2D'>
-    # print('allplotsData[index]', allplotsData[index][0])
-    # print(type(allplotsData[index][0]))
-    allplotsData[index][0].set_visible(not allplotsData[index][0].get_visible())
-
-    ### may need to handle inside the order in artist to deal with error bar ###
-    # https://stackoverflow.com/questions/19470104/python-matplotlib-errorbar-legend-picking
-    # allplotsErrorbar[index].set_visible(not allplotsErrorbar[index].get_visible())
-
-    # update legend and do not show the lines not shown, but label still exists
+    allplotsMatplotlib[index][0].set_visible(not allplotsMatplotlib[index][0].get_visible())
+    # update legend and do not show the lines not checked on box (label still exists)
     ax_1[0].legend(loc='best', fontsize='small')
-    # lgd = ax_1[0, 0].legend()
-    # lgd.set_visible(True)
 
     plt.draw()
 
 chxbox.on_clicked(set_visible)
 
-### check buttons to make visible/not visible in ax_1[0, 1] end ###
+### check buttons to make visible/not visible in ax_1[1] end ###
 
 
 ### error bar plot part ####
+# get_visible function does not handle error bar that is in Artist.
+# show/no show with error bar is handled by re-plotting the selected entry
 
 fig_2, ax_2 = plt.subplots()
 ax_2.set_title('Qz vs. Intensity (error bar)')
 ax_2.set_xlabel('Qz ')
 ax_2.set_ylabel('Intensity ')
 
-textBoxLocation = fig_2.add_axes([0.05, 0.05, 0.05, 0.05])
-### make sure input has comma after number for single entry ###
-textBox = TextBox(textBoxLocation, 'input')
+# text box location
+textBoxLocation = fig_2.add_axes([0.2, 0.94, 0.1, 0.05])
+textBox = TextBox(textBoxLocation, 'selected number' )
+# !!! make sure input has comma after number for single entry !!! #
+ax_2.text(0,1, 'type comma after a number to show one data only')
 
 def submit(expression):
     ax_2.clear()
-    selectedEntry = list(eval(expression))
-    print('selectedEntry', selectedEntry)
-    print(type(selectedEntry))
-
-    selectedPlotsData = []
-    for i in range(len(selectedEntry)):
-        # print('i in submit for loop', i)
-        # print('selectedEntry[i]', selectedEntry[i])
-        erroriData = ax_2.errorbar(dataToPlot[selectedEntry[i]][0], dataToPlot[selectedEntry[i]][1], label='entry' + str(selectedEntry[i]), yerr=dataToPlot[selectedEntry[i]][2], color=next(colors))
-        selectedPlotsData.append(erroriData)
-        ax_2.legend(loc='best')
-
+    # redefine axes settings
     ax_2.set_title('Qz vs. Intensity (error bar)')
     ax_2.set_xlabel('Qz ')
     ax_2.set_ylabel('Intensity ')
-    # error below
-    # ax_1[1, 0].ticklabel_format(axis='both', style='scientific')
     ax_2.semilogy()
-    # ax_1[1, 0].autoscale_view()
+    # text inside axis goes away with clear ()
+    ax_2.text(0, 1, 'type comma after a number to show one data only')
 
-    # below does not work
+    selectedEntry = list(eval(expression))
+
+    # select entry number tped in the box
+    for i in range(len(selectedEntry)):
+        ax_2.errorbar(dataToPlot[selectedEntry[i]][0], dataToPlot[selectedEntry[i]][1], label='entry' + str(selectedEntry[i]), yerr=dataToPlot[selectedEntry[i]][2], color=next(colors))
+        ax_2.legend(loc='best')
+
     plt.draw()
 
 textBox.on_submit(submit)
@@ -153,47 +150,39 @@ textBox.on_submit(submit)
 ### residuals plot part ####
 fig_3, ax_3 = plt.subplots()
 
-entryForResidualsLocation = fig_3.add_axes([0.5, 0.05, 0.05, 0.05])
-### make sure input has comma after number for single entry ###
-entryForResiduals = TextBox(entryForResidualsLocation, 'selected entry number')
+entryForResidualsLocation = fig_3.add_axes([0.3, 0.9, 0.05, 0.05])
+
+entryForResiduals = TextBox(entryForResidualsLocation, 'entry two numbers to compare')
 ax_3.set_title('Qz vs. Residual')
 ax_3.set_xlabel('Qz ')
-ax_3.set_ylabel('Residual: 2(S1-S2)/(E1 + E2)')
+ax_3.set_ylabel('Residual: 2(S1-S2)/(E1 + E2)', color='r')
+ax_3_2 = ax_3.twinx()
+ax_3_2.tick_params(axis='y', labelcolor='b')
+ax_3_2.set_ylabel('Relative Differences', color='b')
+### make sure input has comma after number for single entry ###
+ax_3.text(0,1, 'format: #1, #2')
 
-# ntryNumberForResidualList = []
+# define calcuation process after box entry
 def getEntryNumber(expression):
     ax_3.clear()
     ax_3.set_title('Qz vs. Residual')
     ax_3.set_xlabel('Qz ')
+    ax_3.text(0, 1, 'format: #1, #2')
 
     entryNumberForResidual = list(eval(expression))
-    # print('entryNumberForResidual', entryNumberForResidual)
-    # print(type(entryNumberForResidual))
-    # entryNumberForResidualList.append(entryNumberForResidual)
-    # return entryNumberForResidual
-
-    # print('entryNumberForResidualList', entryNumberForResidualList)
 
     residualsList = []
     relativeDifferencesList = []
 
     # len(dataList[a]) is number of rows in each entry
 
-    # dataToPlot[list of "Qz"][list of "Intensity"][list of "uncertainty"]
+    # dataToPlot format : [[[entry1 "Qz"][entry1 "Intensity"][entry1 "uncertainty"]][[entry2 "Qz"]....]...]
     for a in range(len(dataToPlot[entryNumberForResidual[0]][0])):
-        # print('entryNumberForResidual[0]', entryNumberForResidual[0])
-        # print('entryNumberForResidual[1]', entryNumberForResidual[1])
-        # print('dataToPlot[entryNumberForResidual[0]]', dataToPlot[entryNumberForResidual[0]][0])
-        # print('dataToPlot[entryNumberForResidual[1]]', dataToPlot[entryNumberForResidual[1]][0])
-
-
         # 2(S1-S2)/(E1+E2)  where S is specular intensity and E is Undertainty
         S1 = dataToPlot[entryNumberForResidual[0]][1][a]
         S2 = dataToPlot[entryNumberForResidual[1]][1][a]
         E1 = dataToPlot[entryNumberForResidual[0]][2][a]
         E2 = dataToPlot[entryNumberForResidual[1]][2][a]
-
-        print('S1, S2, E1, E2', S1, S2, E1, E2)
 
         try:
             residual = 2 * (S1 - S2) / (E1 + E2)
@@ -201,28 +190,22 @@ def getEntryNumber(expression):
             residualsList.append(residual)
             relativeDifferencesList.append(relativeDifference)
         except:
-            # if E1+E2 = 0 or S1+S2 = 0
+            # if E1+E2 = 0 or S1+S2 = 0, cannot be divided by 0. Put 0 in list since there is no difference.
             residualsList.append(0)
             relativeDifferencesList.append(0)
 
     ## calculation checked with first and last of 3-4 rows of data set##
 
-    # print('residualsList', residualsList)
-    # print('relativeDifferencesList', relativeDifferencesList)
-
     ax_3.set_ylabel('Residual:2(S1-S2)/(E1+E2)', color = 'r')
     ax_3.tick_params(axis='y', labelcolor='r')
-    # TODO change line width
-    residualsPlot = ax_3.plot(dataToPlot[entryNumberForResidual[0]][0], residualsList, color='r', linewidth=0.5)
+    # residualsPlot
+    ax_3.plot(dataToPlot[entryNumberForResidual[0]][0], residualsList, color='r', linewidth=0.5)
 
     ax_3_2 = ax_3.twinx()
     ax_3_2.tick_params(axis='y', labelcolor='b')
     ax_3_2.set_ylabel('Relative Differences', color='b')
-
-    # TODO change line width
-    relativeDifferencesPlot = ax_3_2.plot(dataToPlot[entryNumberForResidual[0]][0], relativeDifferencesList, color='b', linewidth=0.5)
-
-    # print('residual plot before draw')
+    # relativeDifferencesPlot
+    ax_3_2.plot(dataToPlot[entryNumberForResidual[0]][0], relativeDifferencesList, color='b', linewidth=0.5)
 
     plt.draw()
 
@@ -233,9 +216,11 @@ entryForResiduals.on_submit(getEntryNumber)
 ### combine data and plot part ###
 
 fig_4, ax_4 = plt.subplots()
+
 entryForCombineDataLocation = fig_4.add_axes([0.8, 0.9, 0.05, 0.05])
-### make sure input has comma after number for single entry ###
 entryForCombineData = TextBox(entryForCombineDataLocation, 'combine data')
+
+ax_3.text(0,1, 'format: #1, #2')
 
 ax_4.set_title('combine data')
 ax_4.set_xlabel('Qz')
@@ -247,9 +232,8 @@ def combineData(expression):
     # entry must be only two selected data (list length must be 2 to make inside for-loop work
     entryNumberForCombineData = list(eval(expression))
 
-    # check how many data to combine
+    # check how many data to combine (take length of list entered in a box)
     numberOfDataToCombine = len(entryNumberForCombineData)
-    print('numberOfDataToCombine', numberOfDataToCombine)
 
     # create empty list to put in combined data
     combinedIntensityList = []
@@ -259,34 +243,22 @@ def combineData(expression):
         intOfOeDataSet = dataToPlot[entryNumberForCombineData[n]][1]
         dataToCombine.append(intOfOeDataSet)
     # taking the right data set, checked by first and last couple of intensity columns of selected data
-    print('dataToCombine', dataToCombine)
 
     # take length of Qz column len(dataToPlot[entryNumberForCombineData[0]][0]), should be the same uf u take other columns
     # for loop appears to be working. entry1+entry2 in previous code with individualCombinedData = (S1+S2)/2 match with current code
     for a in range(len(dataToPlot[entryNumberForCombineData[0]][0])):
-        print('len(dataToPlot[entryNumberForCombineData[0]][0])', len(dataToPlot[entryNumberForCombineData[0]][0]))
 
+        # TODO change to correct equation for combining data
         sumData = 0
         for n in range(numberOfDataToCombine):
             # combine the values in the same row
             oneData = dataToPlot[entryNumberForCombineData[n]][1][a]
-            #print('dataToPlot[entryNumberForCombineData[n]][1]', dataToPlot[entryNumberForCombineData[n]][1][a])
-            #print(type(oneData))
-            print('oneData', oneData)
             sumData = float(sumData) + float(oneData)
-            print('sumData', sumData)
-            #print('sumData', sumData)
-
-            #print('n', n)
 
         # combine n number of data points and divide by n
-        print('numberOfDataToCombine after for n ...', numberOfDataToCombine)
         indivisualCombinedIntensity = sumData/numberOfDataToCombine
-        print('indivisualCombinedIntensity', indivisualCombinedIntensity)
 
         combinedIntensityList.append(indivisualCombinedIntensity)
-
-    print('combinedIntensityList', combinedIntensityList)
 
     # set aces label and semi log scale for y
     ax_4.set_xlabel('Qz')
@@ -303,9 +275,9 @@ def combineData(expression):
         # text format: str(Qz) + (space) + str(Intensity)
         oneLine= str(dataToPlot[entryNumberForCombineData[0]][0][a]) + ' ' + str(combinedIntensityList[a])
         combinedQzIntList.append(oneLine)
-    print('combinedQzIntList', combinedQzIntList)
 
     # create a folder  for combined data text if it doesn't exist
+    ### CHANGE DIRECTORY HERE ###
     parentDirectory = 'C:/Users/saya6/Documents/NCNR/kineticsDataAnalizer'
     folderName = sampleName + '_Combined'
     path = os.path.join(parentDirectory, folderName)
@@ -315,7 +287,7 @@ def combineData(expression):
         print(error)
         pass
     # make folder directory
-    folderDirectory = 'C:/Users/saya6/Documents/NCNR/kineticsDataAnalizer/' + folderName + '/'
+    folderDirectory = parentDirectory + '/' + folderName + '/'
 
 
     # combined entry names
@@ -323,19 +295,12 @@ def combineData(expression):
     combinedEntryNames = '_entry'
     # for two dataset entry, range is range(2), takes position 0 ~ 2.
     for n in range(numberOfDataToCombine):
-        print('n', n)
-
-        print('range(numberOfDataToCombine-1)', range(numberOfDataToCombine))
         if n < numberOfDataToCombine-1:
-            print('n begining of  if', n)
+            # append data entry number after '_entry' with comma
             combinedEntryNames = combinedEntryNames + str(entryNumberForCombineData[n]) + ','
-            print('n end of  if', n)
-            print('numberOfDataToCombine-1', numberOfDataToCombine)
         elif n == numberOfDataToCombine-1:
-            print('in elif')
+            # append number without comma for the end of the entry number
             combinedEntryNames = combinedEntryNames + str(entryNumberForCombineData[n])
-
-    print('combinedEntryNames', combinedEntryNames)
 
 
     # export combnied data into text file
